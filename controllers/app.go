dc6ac5829108b632084d981390a3e3e2193ed203
Ad3bay0c <aclintonadebayo@gmail.com>
2021-10-25 00:01:29 +0100

    Application Struct Set up

--- controllers/app.go
+++ controllers/app.go
@@ -1,3 +1,14 @@
 package controllers
 
+import (
+	"github.com/Ad3bay0c/dockersAndGormTesting/db"
+	"github.com/gin-gonic/gin"
+)
 
+type Application struct {
+	DB  *db.PostgreSql
+}
+
+func (app *Application) Index(c *gin.Context) {
+	c.JSON(200, gin.H{"Message": "Working Perfectly"})
+}
