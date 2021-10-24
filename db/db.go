980ccc1894a390ea40aeea54c52a89f9e3cc1dc4
Ad3bay0c <aclintonadebayo@gmail.com>
2021-10-24 23:53:10 +0100

    Database connection added

---
+++ db/db.go
@@ -0,0 +1,27 @@
+package db
+
+import (
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+	"os"
+)
+
+type PostgreSql struct {
+	Db *gorm.DB
+}
+func (pq *PostgreSql) DbConnection()  {
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable TimeZone=Africa/Lagos",
+		os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_DB"))
+	db, err := gorm.Open(postgres.Open(dsn))
+	if err != nil {
+		log.Fatalf("Error: %v", err.Error())
+	}
+	pq.Db = db
+}
+
+func (pq *PostgreSql) AutoMigrate() {
+
+}
