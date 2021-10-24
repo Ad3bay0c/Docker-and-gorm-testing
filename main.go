1161a0dc09f7300b77841a3ea45fc7f2bd099150
Ad3bay0c <aclintonadebayo@gmail.com>
2021-10-24 23:49:50 +0100

    Main file Created and the routes set up

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Ad3bay0c/dockersAndGormTesting/db"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Printf("Error: %v", err.Error())
+	}
+	router := gin.Default()
+
+	router.GET("/", func(context *gin.Context) {
+		context.JSON(200, gin.H{"Message": "Working Perfectly"})
+	})
+	PORT := fmt.Sprintf(":%s",os.Getenv("PORT"))
+	if PORT == ":" {
+		PORT += "8080"
+	}
+	server := &http.Server{
+		Addr: PORT,
+		Handler: router,
+	}
+	db.DbConnection()
+	log.Println("Working perfectly well")
+	err = server.ListenAndServe()
+	if err != nil {
+		panic(err.Error())
+	}
+}
