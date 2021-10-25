dbbdc5b19cb0ad3a12da54b2b5c805aa91fa8def
Ad3bay0c <aclintonadebayo@gmail.com>
2021-10-25 08:20:26 +0100

    Author struct added

--- models/user.go
+++ models/user.go
@@ -1,9 +1,23 @@
 package models
 
-import "gorm.io/gorm"
+import "time"
 
 type User struct {
-	gorm.Model
-	FirstName string
+	ID        string	`gorm:"primaryKey"`
+	FirstName string	`gorm:"size:255;unique"`
 	LastName  string
+	Author    Author		`gorm:"embedded"`
+	CreatedAt time.Time	`gorm:"constraint:OnUpdate:CASCADE,onDelete:SET NULL"`
+	updatedAt time.Time	`gorm:"constraint:OnUpdate:CASCADE,onDelete:SET NULL"`
+}
+
+type Author struct {
+	Email	string	`gorm:"unique"`
+	Password	string
+}
+
+type Books struct {
+	ID	string	`gorm:"primaryKey"`
+	UserId	string
+	User	User	`gorm:"foreignKey:UserId"`
 }
